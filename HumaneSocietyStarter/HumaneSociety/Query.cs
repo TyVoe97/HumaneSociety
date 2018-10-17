801081ae2de9c4ea306e66d6f0208759af228e07
TyVoe97 <tydud2016@gmail.com>
2018-10-17 08:55:36 -0500

    add project files

---
+++ HumaneSocietyStarter/HumaneSociety/Query.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    public static class Query
+    {
+        HumaneSocietyDataContext db = new HumaneSocietyDataContext();
+
+    }
+}
