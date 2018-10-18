8d73f7d750cbe626fd607db640e5383743eaa522
ashraf quraishi <ashrafquraishi4@gmail.com>
2018-10-18 07:43:42 -0500

    get client function made in query class

--- HumaneSocietyStarter/HumaneSociety/Query.cs
+++ HumaneSocietyStarter/HumaneSociety/Query.cs
@@ -8,7 +8,28 @@ namespace HumaneSociety
 {
     public static class Query
     {
-        HumaneSocietyDataContext db = new HumaneSocietyDataContext();
+      public static  HumaneSocietyDataContext database = new HumaneSocietyDataContext();
 
+        public static Client GetClient(string userName, string password)
+        {
+            var Requireddata = (from x in database.Clients
+                                where x.UserName == userName && x.Password == password
+                                select x).Single();
+            return Requireddata;
+        }
+          //database.SubmitChanges();  
+        //GetUserAdoptionStatus
+        //GetAnimalByID
+        //Adopt
+        // SearchForAnimalByMultipleTraits
+        //RetrieveClients
+        //GetStates
+        // AddNewClient
+        //updateClient
+        // UpdateUsername
+        //UpdateEmail
+        //UpdateAddress
+        //UpdateFirstName
+        //UpdateLastName
     }
 }
