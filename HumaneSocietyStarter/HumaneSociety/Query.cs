eb9596bcd83107972ddb4e709e31cc32e527682b
gjjuliex <12baysidejgjx@gmail.com>
2018-10-19 09:28:53 -0500

    fixed formatting by getroom (just a spacing)

--- HumaneSocietyStarter/HumaneSociety/Query.cs
+++ HumaneSocietyStarter/HumaneSociety/Query.cs
@@ -114,7 +114,7 @@ namespace HumaneSociety
 
         public static Room GetRoom(int animalId, string rooms)
         {
-            var room =(from r in db.Rooms
+            var room = (from r in db.Rooms
                        where r.AnimalId == animalId
                        select r).FirstOrDefault();
 
